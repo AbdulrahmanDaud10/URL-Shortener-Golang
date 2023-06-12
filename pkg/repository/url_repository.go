4535fbb865a158c012e3f4882d70d6d66321f259
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2023-06-12 16:03:33 +0300

    Refactor

--- pkg/repository/url_repository.go
+++ pkg/repository/url_repository.go
@@ -9,7 +9,11 @@ import (
 	"gorm.io/gorm"
 )
 
-func Create(db *gorm.DB, ctx *gin.Context) {
+type Store struct {
+	Db *gorm.DB
+}
+
+func (s *Store) Create(ctx *gin.Context) {
 	var url api.URL
 	if err := ctx.ShouldBindJSON(&url); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Bad request"})
@@ -24,7 +28,7 @@ func Create(db *gorm.DB, ctx *gin.Context) {
 
 	// Insert the long URL into the database and get the generated ID
 	var Id int
-	err := db.Raw("INSERT INTO urls(long_url) VALUES($1) RETURNING id", url.LongURL).Scan(&Id)
+	err := s.Db.Raw("INSERT INTO urls(long_url) VALUES($1) RETURNING id", url.LongURL).Scan(&Id)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Database error"})
 		return
@@ -34,7 +38,7 @@ func Create(db *gorm.DB, ctx *gin.Context) {
 	url.ShortURL = api.Base62Encode(Id)
 
 	// Update the record with the short URL
-	err = db.Exec("UPDATE urls SET short_url = $1 WHERE id = $2", url.ShortURL, Id)
+	err = s.Db.Exec("UPDATE urls SET short_url = $1 WHERE id = $2", url.ShortURL, Id)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Database error"})
 		return
@@ -47,4 +51,16 @@ func Create(db *gorm.DB, ctx *gin.Context) {
 func URLValidityCheck(urlString string) bool {
 	u, err := url.Parse(urlString)
 	return err == nil && u.Scheme != "" && u.Host != ""
+}
+
+// Check if cache is available
+func (store *Store) CheckCacheAvailability() {
+	url := api.URL{}
+
+	id := api.Base62Decode(url.ShortURL)
+
+	err := store.Db.Exec("SELECT long_url FROM urls WHERE id=$1", id)
+	if err != nil {
+		return
+	}
 }
